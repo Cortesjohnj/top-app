a8579695439f143a2f90ada3fe41958660098b3a
Juan <juanan0309@gmail.com>
2021-08-29 22:05:20 -0500

    Add variable to Mock Data and Adopted message to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import "../assets/styles/Card.css";
 
 const Card = (props) => {
-  const { _id, name, description, photo_url } = props;
+  const { _id, name, description, photo_url, adopted } = props;
   return (
     <div className="card-list-item">
+      {adopted && (
+        <div className="card-list-message">
+          <p>Adopted</p>
+        </div>
+      )}
       <img className="card-list-item__image" src={photo_url} alt="Pet image" />
       <div className="card-list-item__details">
         <h3 className="card-list-item__details--title">{name}</h3>
