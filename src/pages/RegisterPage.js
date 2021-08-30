682fb07536848aab6dbba7a13411dab64bfc29ea
Diego Plata <diegooplata@gmail.com>
2021-08-30 11:44:05 -0500

    Improve form validation before submitting it

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react'
-import { useForm } from 'react-hook-form'
-import '../assets/styles/RegisterForm.css'
-import { PrimaryButton } from '../components/PrimaryButton'
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+import "../assets/styles/RegisterForm.css";
+import { PrimaryButton } from "../components/PrimaryButton";
 
 function RegisterPage() {
   const {
@@ -9,75 +9,75 @@ function RegisterPage() {
     formState: { errors },
     handleSubmit,
     watch,
-  } = useForm()
-  const password = useRef({})
-  password.current = watch('password', '')
+  } = useForm({ mode: "onBlur" });
+  const password = useRef({});
+  password.current = watch("password", "");
 
   const onSubmit = (data, e) => {
-    e.target.reset()
-    console.log(data)
-  }
+    e.target.reset();
+    console.log(data);
+  };
 
   return (
-    <section className='register'>
-      <div className='register__container'>
+    <section className="register">
+      <div className="register__container">
         <h2>Sign up to continue:</h2>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className='register__container--form'
+          className="register__container--form"
         >
-          {errors?.firstName?.type === 'required' && (
+          {errors?.firstName?.type === "required" && (
             <p>*First name is required</p>
           )}
-          {errors?.firstName?.type === 'maxLength' && (
+          {errors?.firstName?.type === "maxLength" && (
             <p>*First name cannot exceed 20 characters</p>
           )}
-          {errors?.firstName?.type === 'pattern' && (
+          {errors?.firstName?.type === "pattern" && (
             <p>*Alphabetical characters only</p>
           )}
           <input
-            type='text'
-            name='firstName'
-            placeholder='First name'
-            className='form__field'
-            {...register('firstName', {
+            type="text"
+            name="firstName"
+            placeholder="First name"
+            className="form__field"
+            {...register("firstName", {
               required: true,
               maxLength: 20,
               pattern: /^[A-Za-z]+$/i,
             })}
           />
 
-          {errors?.lastName?.type === 'required' && (
+          {errors?.lastName?.type === "required" && (
             <p>*Last name is required</p>
           )}
-          {errors?.lastName?.type === 'maxLength' && (
+          {errors?.lastName?.type === "maxLength" && (
             <p>*First name cannot exceed 20 characters</p>
           )}
-          {errors?.lastname?.type === 'pattern' && (
+          {errors?.lastname?.type === "pattern" && (
             <p>*Alphabetical characters only</p>
           )}
           <input
-            type='text'
-            placeholder='Last name'
-            name='lastName'
-            className='form__field'
-            {...register('lastName', {
+            type="text"
+            placeholder="Last name"
+            name="lastName"
+            className="form__field"
+            {...register("lastName", {
               required: true,
               maxLength: 20,
               pattern: /^[A-Za-z]+$/i,
             })}
           />
 
-          {errors?.email?.type === 'required' && <p>*Email is required</p>}
-          {errors?.email?.type === 'pattern' && (
+          {errors?.email?.type === "required" && <p>*Email is required</p>}
+          {errors?.email?.type === "pattern" && (
             <p>*Please enter a valid email.</p>
           )}
           <input
-            type='email'
-            placeholder='Email'
-            name='email'
-            className='form__field'
-            {...register('email', {
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="form__field"
+            {...register("email", {
               required: true,
               pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
             })}
@@ -107,47 +107,47 @@ function RegisterPage() {
               minLength: 7,
             })}
           /> */}
-          {errors?.password?.type === 'required' && (
+          {errors?.password?.type === "required" && (
             <p>*You must specify a password</p>
           )}
-          {errors?.password?.type === 'pattern' && (
+          {errors?.password?.type === "pattern" && (
             <p>
               *Password must be at least 8 characters, one uppercase with one
               lowercase & one numeric character
             </p>
           )}
           <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            {...register('password', {
+            type="password"
+            placeholder="Password"
+            name="password"
+            {...register("password", {
               required: true,
               pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/,
             })}
           />
-          {errors?.confirmPassword?.type === 'required' && (
+          {errors?.confirmPassword?.type === "required" && (
             <p>*You must confirm the password</p>
           )}
           <input
-            type='password'
-            placeholder='Confirm password'
-            name='confirmPassword'
-            {...register('confirmPassword', {
+            type="password"
+            placeholder="Confirm password"
+            name="confirmPassword"
+            {...register("confirmPassword", {
               required: true,
               validate: value =>
-                value === password.current || 'The passwords do not match',
+                value === password.current || "The passwords do not match",
             })}
           />
           <h3>Sign up as:</h3>
-          {errors?.rol?.type === 'required' && <p>*You must select one</p>}
-          <div className='register__container--form--options'>
+          {errors?.rol?.type === "required" && <p>*You must select one</p>}
+          <div className="register__container--form--options">
             <label>
               User
               <input
-                type='radio'
-                name='user'
-                value='User'
-                {...register('rol', {
+                type="radio"
+                name="user"
+                value="User"
+                {...register("rol", {
                   required: true,
                 })}
               />
@@ -155,10 +155,10 @@ function RegisterPage() {
             <label>
               Foundation
               <input
-                type='radio'
-                name='foundation'
-                value='Foundation'
-                {...register('rol', {
+                type="radio"
+                name="foundation"
+                value="Foundation"
+                {...register("rol", {
                   required: true,
                 })}
               />
@@ -168,14 +168,14 @@ function RegisterPage() {
             Choose a profile picture:
           </label>
           <input type='file' name='profilePic' id='fileUpload' /> */}
-          <PrimaryButton children={'Register'} />
+          <PrimaryButton children={"Register"} />
         </form>
         <h4>
-          Already a member? <a href='/'>Sign in </a>
+          Already a member? <a href="/">Sign in </a>
         </h4>
       </div>
     </section>
-  )
+  );
 }
 
-export { RegisterPage }
+export { RegisterPage };
