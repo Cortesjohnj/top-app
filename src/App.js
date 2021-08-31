6b80ae774f64b1f55386197da8f1655a20e26472
Jose Alejandro <javera701@gmail.com>
2021-08-31 13:11:20 -0500

    adding pet foundations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-function App() {
+import "./App.css"
+import MockData from "./MockData"
+import { useState } from "react";
+
+
+const Image = (props) => {
+    let [isOpen, setIsOpen] = useState(false);
+    const handleOpenImage = () => {
+        setIsOpen(!isOpen);
+      };
+    return <figure className="photo" >
+      <img className="image" src={props.photo_url} alt="pet" onClick= {handleOpenImage} />
+      <h1 className="title"> {props.name} </h1>
+          <figcaption className="text"> <b>Dirección</b>: {props.address} <br/><b>Email</b>: {props.email} <br/>
+            <b>Teléfono</b>: {props.phone} </figcaption>
+       </figure>
+}
+
+const Menu = () =>{
+  let rows = []
+  for (let i = 0; i < MockData.foundations.length; i++){
+    let foundation = MockData.foundations[i]
+    rows.push(  <Image name={foundation.name} address={foundation.address}
+      email={foundation.email} phone={foundation.phone_number} photo_url={foundation.photo_url}> </Image>)
+    
+  }
+  return <div className= "container">{rows} </div>
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. vamos a ver si carga
-          de nuevo?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Menu />
     </div>
-  );
+  )
 }
 
 export default App;
