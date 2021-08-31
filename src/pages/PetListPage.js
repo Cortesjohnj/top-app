0b2484d9481b7ff8567a7e0dbb57c55803794b0b
Juan <juanan0309@gmail.com>
2021-08-31 10:38:56 -0500

    Add pets filtered by foundation

--- src/pages/PetListPage.js
+++ src/pages/PetListPage.js
@@ -1,16 +1,28 @@
+import { useParams } from "react-router";
+
 import CardList from "../components/CardList";
 import PetCard from "../components/PetCard";
+import "../assets/styles/PetListPage.css";
 
 import MockData from "../MockData";
 
 const PetListPage = () => {
   const { pets } = MockData;
-
+  const { id: foundation_id } = useParams();
+  const filteredPets = pets.filter(
+    (item) => item.foundation_id === +foundation_id
+  );
   return (
     <CardList title="Are you looking for a new friend?">
-      {pets.map((item) => (
-        <PetCard key={item._id} {...item} redirectUrl="/request/" />
-      ))}
+      {filteredPets.length > 0 ? (
+        filteredPets.map((item) => (
+          <PetCard key={item._id} {...item} redirectUrl="/request/" />
+        ))
+      ) : (
+        <h1 className="no-pets-message">
+          No pets available for this foundation
+        </h1>
+      )}
     </CardList>
   );
 };
