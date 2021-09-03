7d41d47f56b96abc0b16ebe6a8b35adc7d1e2bb3
John <cortesjohnj@gmail.com>
2021-09-03 17:02:28 -0500

    Create a Spinner component and add different styles and animations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
+import './assets/styles/GeneralStyles.css';
+import Spinner from './components/Spinner';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. vamos a ver si carga
-          de nuevo?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Spinner />;
 }
 
 export default App;
