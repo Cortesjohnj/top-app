023a7535bddbab89d4538614fbbe0ef7e2c7bc68
John <cortesjohnj@gmail.com>
2021-09-03 17:15:54 -0500

    add React suspense to all the app and Spinner as fallback component in React suspense

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { Suspense } from 'react';
 import './assets/styles/GeneralStyles.css';
 import Spinner from './components/Spinner';
 
 function App() {
-  return <Spinner />;
+  return <Suspense fallback={<Spinner />}></Suspense>;
 }
 
 export default App;
