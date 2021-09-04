cda111e6204cf85e13252ae401fd9e662484347a
John <cortesjohnj@gmail.com>
2021-09-03 19:15:48 -0500

    Create notFound page, add styles, and with react-router-dom add a link to the button that sends to home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import './assets/styles/GeneralStyles.css';
+import NotFound from './components/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. vamos a ver si carga
-          de nuevo?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NotFound />
+    </Router>
   );
 }
 
