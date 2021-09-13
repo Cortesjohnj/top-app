4da57f8883039df9c77d8887b03740fd1207d974
Jose Alejandro <javera701@gmail.com>
2021-09-13 10:19:36 -0500

    Adding mock data and custom axios

--- src/components/FoundationsMenu.js
+++ src/components/FoundationsMenu.js
@@ -1,18 +1,33 @@
-import FoundationsImage from "../components/FoundationsImage"
-import axios from "axios";
-import React from "react"
+import FoundationsImage from "../components/FoundationsImage";
+import customAxios from "../axios";
+import { useState, useEffect } from "react";
 
 const FoundationsMenu = () => {
+  const [foundations, setFoundations] = useState([]);
+  useEffect(() => {
+    customAxios
+      .get("http://localhost:8080/foundations")
+      .then((response) => setFoundations(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  if (!foundations) return null;
+  return (
+    <div className="container-foundations ">
+      {foundations.map((foundation) => (
+        <FoundationsImage
+          name={foundation.name}
+          address={foundation.address}
+          email={foundation.email}
+          phone={foundation.phoneNumber}
+          photo_url={foundation.photoUrl}
+          id={foundation._id}
+          key={foundation._id}
+        >
+          {" "}
+        </FoundationsImage>
+      ))}{" "}
+    </div>
+  );
+};
 
-  const [foundations, setFoundations] = React.useState([])
-  React.useEffect(() => {
-      axios.get("http://localhost:8080/foundations")
-      .then(response => setFoundations(response.data))
-      .catch(error => console.log(error))
-  }, [])
-  if (!foundations) return null
-  return <div className= "container-foundations ">{ foundations.map(foundation => <FoundationsImage name={foundation.name} address={foundation.address}
-      email={foundation.email} phone={foundation.phoneNumber} photo_url={foundation.photoUrl} id = {foundation._id} key= {foundation._id}> </FoundationsImage>)} </div>
-}
-
-export default FoundationsMenu
+export default FoundationsMenu;
