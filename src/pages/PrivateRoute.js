795d6cc77ada1ccd5807f77b6caf35b839cf9282
Diego Plata <diegooplata@gmail.com>
2021-09-14 18:16:18 -0500

    Fixed private route render depending on auth status

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
-import { AUTHENTICATED } from "../store/actions";
+import { AUTHENTICATED, LOADING } from "../store/actions";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const status = useSelector(state => state.status);
   console.log(status);
+
+  if (status === LOADING) return <p>Loading ....</p>;
 
   return (
     <Route
