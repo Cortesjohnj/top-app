d6fcd4ef9c9070a9f8ca405e7a1be897d3ebb838
John <cortesjohnj@gmail.com>
2021-09-16 16:42:04 -0500

    Adjust css styles

--- src/components/slider2/SliderData2.js
+++ src/components/slider2/SliderData2.js
@@ -1,26 +1,27 @@
+import Dog3 from "../../assets/images/security colored.svg";
 export const SliderData2 = [
   {
-    image: '',
-    name: 'Boxers',
+    image: Dog3,
+    name: "Boxers",
   },
 
   {
-    image: '',
-    name: 'French Bulldog',
+    image: "",
+    name: "French Bulldog",
   },
 
   {
-    image: '',
-    name: 'Cats',
+    image: "",
+    name: "Cats",
   },
 
   {
-    image: '',
-    name: 'Cats',
+    image: "",
+    name: "Cats",
   },
 
   {
-    image: '',
-    name: 'Dogs',
+    image: "",
+    name: "Dogs",
   },
 ];
