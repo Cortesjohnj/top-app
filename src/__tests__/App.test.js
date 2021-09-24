fcf53698302279c9eb87861a019acaf1d06a85ea
Juan <juanan0309@gmail.com>
2021-09-23 20:42:19 -0500

    Add first tests for petManagePage

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -85,3 +85,65 @@ test("PetListPage redirects to login if not authenticated", async () => {
     expect(screen.queryByText(/new friend/i)).not.toBeInTheDocument()
   );
 });
+
+test("renders PetManagePage component", async () => {
+  axios.get.mockResolvedValueOnce({
+    data: {
+      _id: "613fecc4e485559caa864add",
+      email: "foundationt@test.com",
+      name: "foundationDiego",
+      role: "foundation",
+    },
+  });
+  axios.get.mockResolvedValueOnce({
+    data: {
+      page: 1,
+      count: 1,
+      pets: [
+        {
+          _id: "6143e2a60d4bc9d25fb9e929",
+          name: "coco1",
+          description: "test pet",
+          photoUrl: ["http://dummyimage.com/119x100.png/cc0000/ffffff"],
+          age: 10,
+          foundationId: "613fecc4e485559caa864add",
+          adopted: false,
+          __v: 0,
+        },
+      ],
+    },
+  });
+  axios.get.mockResolvedValueOnce({
+    data: [
+      {
+        _id: "614a107a8e075232b9776e95",
+        userId: "6144b9b1b085aa415714f515",
+        petId: "6143e2a60d4bc9d25fb9e929",
+        description: "Test description",
+        responseStatus: "pending",
+      },
+    ],
+  });
+  localStorage.setItem(AUTHORIZATION, "12345");
+  history.push("/pets/6143e2a60d4bc9d25fb9e929/manage");
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  await waitFor(() =>
+    expect(screen.getByTestId("petManagePage")).toBeInTheDocument()
+  );
+});
+
+test("PetManagePage redirects to login if not authenticated", async () => {
+  history.push("/pets/6143e2a60d4bc9d25fb9e929/manage");
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  await waitFor(() =>
+    expect(screen.queryByTestId("petManagePage")).not.toBeInTheDocument()
+  );
+});
