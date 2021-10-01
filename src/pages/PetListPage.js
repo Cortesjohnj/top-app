fd663c3be1fa453a8a2995f53e8ddc71d3cdec9c
Juan <juanan0309@gmail.com>
2021-10-01 14:24:45 -0500

    Add page queryString to URL

--- src/pages/PetListPage.js
+++ src/pages/PetListPage.js
@@ -9,14 +9,16 @@ import { listPets, listFoundationRequests } from "../store/actionCreators";
 import CardList from "../components/CardList";
 import PetCard from "../components/PetCard";
 import PaginationButtons from "../components/PaginationButtons";
+import history from "../history";
+
 import "../assets/styles/PetListPage.css";
 
 const PetListPage = () => {
   const { id: foundationId } = useParams();
   let redirectUrl = "";
   const dispatch = useDispatch();
-  const { pets, petListInfo } = useSelector((state) => state);
-  const { user } = useSelector((state) => state);
+  const { pets, petListInfo } = useSelector(state => state);
+  const { user } = useSelector(state => state);
 
   useEffect(() => {
     dispatch(listPets(foundationId, 1));
@@ -38,17 +40,25 @@ const PetListPage = () => {
 
   const handleNextPage = () => {
     dispatch(listPets(foundationId, petListInfo.page + 1));
+    history.push({
+      pathname: `/foundations/${foundationId}/pets`,
+      search: `?page=${petListInfo.page + 1}`,
+    });
   };
 
   const handlePreviousPage = () => {
     dispatch(listPets(foundationId, petListInfo.page - 1));
+    history.push({
+      pathname: `/foundations/${foundationId}/pets`,
+      search: `?page=${petListInfo.page - 1}`,
+    });
   };
 
   return (
     <div className="background-container">
       <CardList title="Are you looking for a new friend?">
         {pets.length > 0 ? (
-          pets.map((item) => (
+          pets.map(item => (
             <PetCard
               key={item._id}
               {...item}
