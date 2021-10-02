99ac1ec25322bfdb67ce2a73d0c76e3eb8bc5882
Diego Plata <diegooplata@gmail.com>
2021-10-02 18:17:04 -0500

    Create upload functionality with cloudinary

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // import { AUTHORIZATION } from "./store/actions";
 
 const customAxios = axios.create({
-  //baseURL: "http://localhost:8080",
-  baseURL: "https://adogta-backend-make-it-real.herokuapp.com",
+  baseURL: "http://localhost:8080",
+  // baseURL: "https://adogta-backend-make-it-real.herokuapp.com",
   // headers: {
   //   Authorization: localStorage.getItem(AUTHORIZATION),
   // },
