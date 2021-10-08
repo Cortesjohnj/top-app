78335c45ffe9b75d9aa0fd6efd228cb1d4a99292
Jose Alejandro <javera701@gmail.com>
2021-10-08 15:50:27 -0500

    delete comments

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,8 @@
 import axios from "axios";
-// import { AUTHORIZATION } from "./store/actions";
 const { REACT_APP_URL } = process.env;
+
 const customAxios = axios.create({
-  // baseURL: "http://localhost:8080",
   baseURL: REACT_APP_URL,
-  // headers: {
-  //   Authorization: localStorage.getItem(AUTHORIZATION),
-  // },
 });
 
 export default customAxios;
